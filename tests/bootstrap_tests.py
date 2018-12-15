594470e0023fd3d7af15c21b1d9b1448ebac9049
Alec Lofquist <alec.lofquist@affirm.com>
2018-12-14 16:59:37 -0800

    fix tests + fix infra-7426

--- tests/bootstrap_tests.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.conf import settings
-settings.configure()
-settings.JENKINS_USER = 'Test'
-settings.JENKINS_PASS = 'Test'
-settings.GRAPHITE_API = 'Test'
-settings.GRAPHITE_USER = 'Test'
-settings.GRAPHITE_PASS = 'Test'
-settings.GRAPHITE_FROM = 'Test'
